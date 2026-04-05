ac74cf66e36e5659581a4ee95216f98e45a1962d
Stéphane Klein <contact@stephane-klein.info>
2026-04-05 22:13:00 +0200

    Fix Tmux Catppuccin install method

--- run_once_before_install-tmux-catppuccin.sh
+++ run_once_before_install-tmux-catppuccin.sh
@@ -2,5 +2,6 @@
 CATPPUCCIN_DIR="$HOME/.config/tmux/plugins/catppuccin"
 if [ ! -d "$CATPPUCCIN_DIR" ]; then
     mkdir -p "$(dirname "$CATPPUCCIN_DIR")"
-    git clone --depth=1 -b v2.1.3 https://github.com/catppuccin/tmux.git "$CATPPUCCIN_DIR"
+    curl -L https://github.com/catppuccin/tmux/archive/refs/tags/v2.1.3.tar.gz | tar -xz -C /tmp
+    mv /tmp/tmux-2.1.3 "$CATPPUCCIN_DIR"
 fi
