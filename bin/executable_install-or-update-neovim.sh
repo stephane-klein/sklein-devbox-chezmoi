62d0e85615a069bd4859fcad8d98ffa093c7cfa6
Stéphane Klein <contact@stephane-klein.info>
2026-04-19 18:29:33 +0200

    Improve progression message in install-or-update-neovim.sh

--- bin/executable_install-or-update-neovim.sh
+++ bin/executable_install-or-update-neovim.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 # ~/bin/install-or-update-neovim.sh
 
-nvim --headless "+Lazy! restore" \
-  -c "MasonToolsInstallSync" \
-  -c "TSInstallSync" \
-  +qa
+echo "Install Lazy packages..."
+nvim --headless "+Lazy! restore" +qa
+echo -e "...Lazy packages installation done\n"
+
+echo "Install Mason packages..."
+nvim --headless -c "MasonToolsInstallSync" +qa
+echo -e "...Mason installation done\n"
+
+echo "Install TreeSitter packages..."
+nvim --headless -c "TSInstallSync" +qa
+echo -e "...TreeSitter installation done\n"
