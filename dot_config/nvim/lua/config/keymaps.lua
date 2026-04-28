78f5f6aa8a5e85d66d190d3016a4ad678f6f0cc5
Stéphane Klein <contact@stephane-klein.info>
2026-04-28 13:18:11 +0000

    Add Navigator.nvim plugin to easily navigate between panes

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -52,10 +52,10 @@ vim.keymap.set('n', '<leader><leader>D', 'gg"_dG', { desc = "Delete all lines wi
 -- }}}
 
 -- Move to windows and split windows {{{
-vim.keymap.set("n", "<Leader><Up>", "<C-W><C-K>", { desc = "Move to up window" })
-vim.keymap.set("n", "<Leader><Left>", "<C-W><C-H>", { desc = "Move to left window" })
-vim.keymap.set("n", "<Leader><Right>", "<C-W><C-L>", { desc = "Move to right window" })
-vim.keymap.set("n", "<Leader><Down>", "<C-W><C-J>", { desc = "Move to down window" })
+vim.keymap.set("n", "<Leader><Up>", "<Cmd>NavigatorUp<CR>", { desc = "Move to up window" })
+vim.keymap.set("n", "<Leader><Left>", "<Cmd>NavigatorLeft<CR>", { desc = "Move to left window" })
+vim.keymap.set("n", "<Leader><Right>", "<Cmd>NavigatorRight<CR>", { desc = "Move to right window" })
+vim.keymap.set("n", "<Leader><Down>", "<Cmd>NavigatorDown<CR>", { desc = "Move to down window" })
 
 vim.keymap.set("n", "<Leader><Leader><Up>", "<Cmd>:sp<CR><C-W><C-K>", { desc = "Split on the top" })
 vim.keymap.set("n", "<Leader><Leader><Left>", "<Cmd>:vs<CR><C-W><C-H>", { desc = "Split on the left" })
