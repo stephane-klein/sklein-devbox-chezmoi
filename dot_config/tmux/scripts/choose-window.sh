0ca99b955d264dd9db24d1a0ac6c05eb2c9e4ec7
Stéphane Klein <contact@stephane-klein.info>
2026-04-29 08:07:34 +0000

    Add Ctrl+X to kill Tmux pane in choose-window command
    Add key binding to delete panes from the fzf Tmux window selector.

--- dot_config/tmux/scripts/choose-window.sh
+++ dot_config/tmux/scripts/choose-window.sh
@@ -5,8 +5,10 @@ TARGET=$(
     -F '#{window_index}.#{pane_index}	#{window_name}	#{pane_title}' \
   | column -t -s $'\t' \
   | fzf --reverse --prompt="window> " \
+        --header="Ctrl-X: kill pane | Enter: select" \
         --preview 'tmux capture-pane -pt {1} -e' \
-        --preview-window=right:60%
+        --preview-window=right:60% \
+        --bind 'ctrl-x:execute-silent(tmux kill-pane -t {1})+reload(tmux list-panes -a -F "#{window_index}.#{pane_index}'$'\t''#{window_name}'$'\t''#{pane_title}")'
 )
 
 [[ -z "$TARGET" ]] && exit 0
