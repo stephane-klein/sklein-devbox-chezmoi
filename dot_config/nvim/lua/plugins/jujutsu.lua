a2522db4b709eee16428604d6cc88132585451bb
Stéphane Klein <contact@stephane-klein.info>
2026-05-04 21:34:29 +0000

    Add Jujutsu Neovim package

--- dot_config/nvim/lua/plugins/jujutsu.lua
+++ dot_config/nvim/lua/plugins/jujutsu.lua
@@ -1,6 +1,22 @@
 return {
-  "evanphx/jjsigns.nvim",
-  config = function()
-    require('jjsigns').setup()
-  end
+  {
+    "evanphx/jjsigns.nvim",
+    config = function()
+      require('jjsigns').setup()
+    end
+  },
+  {
+    "nicolasgb/jj.nvim",
+    version = "*",
+    dependencies = {
+      "esmuellert/codediff.nvim",
+    },
+    config = function()
+      require("jj").setup({
+        diff = {
+            backend = "codediff"
+        }
+      })
+    end,
+  }
 }
