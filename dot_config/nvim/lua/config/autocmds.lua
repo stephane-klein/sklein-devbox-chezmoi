20d48bfdce5894fa47da14ada06b87b596e140ae
Stéphane Klein <contact@stephane-klein.info>
2026-05-14 09:47:05 +0000

    Disable LazyVim forced spell activation
    Remove the `lazyvim_wrap_spell` autocmd group to prevent
    LazyVim from automatically enabling spell checking on
    certain filetypes and re-activating it during the session.

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -5,8 +5,8 @@
 -- with `vim.api.nvim_create_autocmd`
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
---
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
 -- Disable window-local directories
 vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
   pattern = "*",
