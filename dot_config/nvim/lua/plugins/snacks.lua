796f04b4b69003d596bc8ca3a420894ab957ddf2
Stéphane Klein <contact@stephane-klein.info>
2026-06-03 13:28:31 +0000

    Display <a-i> <a-h> hints in Neovim Snacks

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -3,38 +3,63 @@ return {
   keys = {
     { "<leader><space>", false }, -- Because I use custom keymap based on <space><space> to split windows
     { "<leader>.",       false }, -- Because I use <leader>. to open floating neo-tree
+    {
+      "<leader>fI",
+      function()
+        LazyVim.pick("files", { ignored = false })()
+      end,
+      desc = "Find Files (Root Dir, no ignored)",
+    },
   },
   ---@type snacks.Config
-  opts = {
-    animate = {
-      enabled = false
-    },
-    scroll = {
-      enabled = false
-    },
-    picker = {
-      layouts = {
-        vertical = {
-          layout = {
-            width = 0.9,
-            height = 0.9
-          }
-        }
+  opts = function(_, opts)
+    vim.api.nvim_set_hl(0, "SnacksPickerToggleHidden", { fg = "#ff8800", bold = true })
+    vim.api.nvim_set_hl(0, "SnacksPickerToggleIgnored", { fg = "#888888", bold = true })
+    return vim.tbl_deep_extend("force", opts or {}, {
+      animate = {
+        enabled = false
       },
-      layout = {
-        preset = "vertical",
+      scroll = {
+        enabled = false
       },
-    },
-    zen = {
-      toggles = {
-        dim = false,
-      }
-    },
-    styles = {
+      picker = {
+        sources = {
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+          grep = {
+            hidden = true,
+          },
+        },
+        toggles = {
+          hidden = { icon = "H" },
+          ignored = { icon = "I" },
+        },
+        layouts = {
+          vertical = {
+            layout = {
+              title = "{title} {live} {flags} | hints: <a-h> <a-i>",
+              width = 0.9,
+              height = 0.9,
+            },
+          },
+        },
+        layout = {
+          preset = "vertical",
+        },
+      },
       zen = {
-        backdrop = { transparent = false, blend = 30 },
-        keys = { q = "close" },
-      }
-    }
-  },
+        toggles = {
+          dim = false,
+        },
+      },
+      styles = {
+        zen = {
+          backdrop = { transparent = false, blend = 30 },
+          keys = { q = "close" },
+        },
+      },
+    })
+  end,
 }
